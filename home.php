8438df40c597ca59a1588dbbf900314d1decd50b
Joey Phan <joseph.phan@whitehatsec.com>
2017-10-24 09:36:35 -0700

    updated home.php

--- home.php
+++ home.php
@@ -1,386 +1,254 @@
-﻿<!DOCTYPE html>
-<html>
-<head>
-    <title>HTML5 Event Calendar</title>
-	<!-- demo stylesheet -->
-    	<link type="text/css" rel="stylesheet" href="media/layout.css" />
+<?php
+$servername = "dbserver.engr.scu.edu";
+$username = "jphan1";
+$password = "sandbox";
+$db_name = "sdb_jphan1";
+?>
 
-        <link type="text/css" rel="stylesheet" href="themes/calendar_g.css" />
-        <link type="text/css" rel="stylesheet" href="themes/calendar_green.css" />
-        <link type="text/css" rel="stylesheet" href="themes/calendar_traditional.css" />
-        <link type="text/css" rel="stylesheet" href="themes/calendar_transparent.css" />
-        <link type="text/css" rel="stylesheet" href="themes/calendar_white.css" />
-        <meta charset="utf-8" />
-        <link rel="icon" type="image/png" href="assets/img/favicon.ico">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Scu Labs</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+    <head>
+        <title>Timetable</title>
+        <style type="text/css">
 
+        table
+        {
+          margin-left: 5%;
+          clear: boht
+        }
 
-        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-        <meta name="viewport" content="width=device-width" />
+        body
+        {
+            font-family: arial;
+        }
 
+        th,td
+        {
+            margin: 0;
+            text-align: center;
+            border-collapse: collapse;
+            outline: 1px solid #e3e3e3;
+        }
 
-        <!-- Bootstrap core CSS     -->
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+        td
+        {
+            padding: 0px 0px;
+        }
 
-        <!-- Animation library for notifications   -->
-        <link href="assets/css/animate.min.css" rel="stylesheet"/>
+      th
+      {
+          background: #9a2035;
+          color: white;
+          padding: 30px 0px 30px 0px;
+      }
 
-        <!--  Light Bootstrap Table core CSS    -->
-        <link href="assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
+      td:hover
+      {
+          cursor: pointer;
+          background: #d1465a;
+          color: white;
+      }
 
+      ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+        }
 
-        <!--  CSS for Demo Purpose, don't include it in your project     -->
-        <link href="assets/css/demo.css" rel="stylesheet" />
+        li {
+            float: left;
+        }
 
+        li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
 
-        <!--     Fonts and icons     -->
-        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-        <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-        <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+        li a:hover {
+            background-color: #111;
+        }
+      </style>
 
-	<!-- helper libraries -->
-	<script src="js/jquery-1.9.1.min.js" type="text/javascript"></script>
+  </head>
 
-	<!-- daypilot libraries -->
-        <script src="js/daypilot/daypilot-all.min.js" type="text/javascript"></script>
+  <?php
 
-</head>
-
-
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
-
-<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-<meta name="viewport" content="width=device-width" />
-
-
-<!-- Bootstrap core CSS     -->
-<link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-
-<!-- Animation library for notifications   -->
-<link href="assets/css/animate.min.css" rel="stylesheet"/>
-
-<!--  Light Bootstrap Table core CSS    -->
-<link href="assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
-
-
-<!--  CSS for Demo Purpose, don't include it in your project     -->
-<link href="assets/css/demo.css" rel="stylesheet" />
-
-
-<!--     Fonts and icons     -->
-<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-<link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
-</head>
-<body>
-
-<div class="wrapper">
-<div class="sidebar" data-color="red" data-image="assets/img/sidebar-6.jpg">
-
-    <!--
-
-        Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
-        Tip 2: you can also add an image using data-image tag
-
-    -->
-
-    <div class="sidebar-wrapper">
-        <div class="logo">
-            <a class="simple-text">
-                Scu Labs
-            </a>
-        </div>
-
-        <ul class="nav">
-            <li class="active">
-                <a href="home.php">
-                    <i class="pe-7s-home"></i>
-                    <p>Home</p>
-                </a>
-            </li>
-            <li>
-                <a href="coen10.php">
-                    <i class="pe-7s-box2"></i>
-                    <p>Coen10</p>
-                </a>
-            </li>
-            <li>
-                <a href="Coen11.html">
-                    <i class="pe-7s-box2"></i>
-                    <p>Coen11</p>
-                </a>
-            </li>
-            <li>
-                <a href="Coen12.html">
-                    <i class="pe-7s-box2"></i>
-                    <p>Coen12</p>
-                </a>
-            </li>
-            <li>
-                <a href="settings.html">
-                    <i class="pe-7s-config"></i>
-                    <p>Settings</p>
-                </a>
-            </li>
-            <li class="active-pro">
-                <a href="support.html">
-                    <i class="pe-7s-users"></i>
-                    <p>Product Support</p>
-                </a>
-            </li>
-
-        </ul>
-    </div>
-</div>
-
-<div class="main-panel">
-    <nav class="navbar navbar-default navbar-fixed">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Dashboard</a>
-            </div>
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav navbar-left">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <i class="fa fa-globe"></i>
-                            <b class="caret hidden-sm hidden-xs"></b>
-                            <span class="notification hidden-sm hidden-xs">5</span>
-                            <p class="hidden-lg hidden-md">
-                                5 Notifications
-                                <b class="caret"></b>
-                            </p>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Notification 1</a></li>
-                            <li><a href="#">Notification 2</a></li>
-                            <li><a href="#">Notification 3</a></li>
-                            <li><a href="#">Notification 4</a></li>
-                            <li><a href="#">Another notification</a></li>
-                        </ul>
-                    </li>
-                </ul>
-
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <p>
-                                Dropdown
-                                <b class="caret"></b>
-                            </p>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">Separated link</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="login.html">
-                            <p>Log out</p>
-                        </a>
-                    </li>
-                    <li class="separator hidden-lg hidden-md"></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+  $conn = new mysqli($servername, $username, $password, $db_name);
 
 
-    <div class="content">
-      <div id="header">
-      </div>
-      <div class="hideSkipLink">
-      </div>
-      <div class="main">
+  if ($conn->connect_error) {
+	        die("Connection failed: " . $conn->connect_error);
+			  }
 
-          <div style="float:left; width: 160px;">
-              <div id="nav"></div>
+
+    $sql = "SELECT * FROM Users";
+    $result = $conn->query($sql);
+	  $table = array();
+	  if ($result->num_rows > 0) {
+		        while($row = $result->fetch_assoc()) {
+					        array_push($table,$row);
+							      }
+				  }
+
+	    mysqli_close($conn);
+
+	    ?>
+
+  <h3>Scu Labs : TA Contact Sheet</h3>
+  <ul>
+    <li><a href="news.asp">Other's Availability</a></li>
+    <li><a href="contact.asp">My Availability</a></li>
+    <li><a href="contact.asp">Professor's View</a></li>
+    <li><a href="about.asp">Logout</a></li>
+  </ul>
+
+  <body>
+    <p> Click on the time slot to find available TAs</p>
+    <br>
+      <table width="90%" align="center" >
+      <div id="head_nav">
+      <tr>
+          <th>Time</th>
+          <th>Mon</th>
+          <th>Tues</th>
+          <th>Wed</th>
+          <th>Thu</th>
+          <th>Fri</th>
+      </tr>
+  </div>
+
+      <tr>
+          <th>09:15 - 12:00</th>
+              <td id="MonLab1">Monday<br>Morning Lab</td>
+              <td id="TueLab1">Tuesday<br>Morning Lab</td>
+              <td id="WedLab1">Wednesday<br>Morning Lab</td>
+              <td id="ThuLab1">Thursday<br>Morning Lab</td>
+              <td id="FriLab1">Friday<br>Morning Lab</td>
           </div>
-          <div style="margin-left: 160px;">
+      </tr>
 
-              <div class="space">
-                  Theme: <select id="theme">
-                      <option value="calendar_default">Default</option>
-                      <option value="calendar_white">White</option>
-                      <option value="calendar_g">Google-Like</option>
-                      <option value="calendar_green">Green</option>
-                      <option value="calendar_traditional">Traditional</option>
-                      <option value="calendar_transparent">Transparent</option>
-                  </select>
-              </div>
-
-              <div id="dp"></div>
+      <tr>
+          <th>02:15 - 5:00</td>
+              <td id="MonLab2">Monday<br>Afternoon Lab</td>
+              <td id="TueLab2">Tuesday<br>Afternoon Lab</td>
+              <td id="WedLab2">Wednesday<br>Afternoon Lab</td>
+              <td id="ThuLab2">Thursday<br>Afternoon Lab</td>
+              <td id="FriLab2">Friday<br>Afternoon Lab</td>
+              <td></td>
           </div>
+      </tr>
 
-          <script type="text/javascript">
+      <tr>
+          <th>05:15 - 08:00</td>
 
-              var nav = new DayPilot.Navigator("nav");
-              nav.showMonths = 3;
-              nav.skipMonths = 3;
-              nav.selectMode = "week";
-              nav.onTimeRangeSelected = function(args) {
-                  dp.startDate = args.day;
-                  dp.update();
-                  loadEvents();
-              };
-              nav.init();
+              <td id="MonLab3">Monday<br>Night Lab</td>
+              <td id="TueLab3">Tuesday<br>Night Lab</td>
+              <td id="WedLab3">Wednesday<br>Night Lab</td>
+              <td id="ThuLab3">Thursday<br>Night Lab</td>
+              <td id="FriLab3">Friday<br>Night Lab</td>
+              <td></td>
 
-              var dp = new DayPilot.Calendar("dp");
-              dp.viewType = "Week";
+          </div>
+      </tr>
 
-              dp.onEventMoved = function (args) {
-                  $.post("backend_move.php",
-                          {
-                              id: args.e.id(),
-                              newStart: args.newStart.toString(),
-                              newEnd: args.newEnd.toString()
-                          },
-                          function() {
-                              console.log("Moved.");
-                          });
-              };
+  </table>
 
-              dp.onEventResized = function (args) {
-                  $.post("backend_resize.php",
-                          {
-                              id: args.e.id(),
-                              newStart: args.newStart.toString(),
-                              newEnd: args.newEnd.toString()
-                          },
-                          function() {
-                              console.log("Resized.");
-                          });
-              };
-
-              // event creating
-              dp.onTimeRangeSelected = function (args) {
-                  var name = prompt("New event name:", "Event");
-                  dp.clearSelection();
-                  if (!name) return;
-                  var e = new DayPilot.Event({
-                      start: args.start,
-                      end: args.end,
-                      id: DayPilot.guid(),
-                      resource: args.resource,
-                      text: name
-                  });
-                  dp.events.add(e);
-
-                  $.post("backend_create.php",
-                          {
-                              start: args.start.toString(),
-                              end: args.end.toString(),
-                              name: name
-                          },
-                          function() {
-                              console.log("Created.");
-                          });
-
-              };
-
-              dp.onEventClick = function(args) {
-                  alert("clicked: " + args.e.id());
-              };
-
-              dp.init();
-
-              loadEvents();
-
-              function loadEvents() {
-                  var start = dp.visibleStart();
-                  var end = dp.visibleEnd();
-
-                  $.post("backend_events.php",
-                  {
-                      start: start.toString(),
-                      end: end.toString()
-                  },
-                  function(data) {
-                      //console.log(data);
-                      dp.events.list = data;
-                      dp.update();
-                  });
-              }
-
-          </script>
-
-          <script type="text/javascript">
-          $(document).ready(function() {
-              $("#theme").change(function(e) {
-                  dp.theme = this.value;
-                  dp.update();
-              });
-          });
-          </script>
-
-      </div>
-      <div class="clear">
-      </div>
-    </div>
-
-    <footer class="footer">
-        <div class="container-fluid">
-            <p class="copyright pull-right">
-                &copy; <script>document.write(new Date().getFullYear())</script> My Smart Mirror
-            </p>
-        </div>
-    </footer>
-
-</div>
-</div>
+<br><br>
+  <h4>Available TA's:</h4>
+  <p id="available_ta_text" ></p>
+  <h4>Email List:</h4>
+  <p id="available_ta_email_list" ></p>
 
 
 </body>
 
-<!--   Core JS Files   -->
-<script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
-<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+<script>
 
-<!--  Checkbox, Radio & Switch Plugins -->
-<script src="assets/js/bootstrap-checkbox-radio-switch.js"></script>
+function myFunction(lab_name) {
+  console.log('Here! Lab Name:',lab_name )
+  var ar = <?php echo json_encode($table) ?>;
+  var email_list = ''
+  var string = ''
+  for (var i in ar){
+    if (ar[i][lab_name] == 'Available' && ar[i]['Type'] == 'TA'){
+      string = string +  ar[i]['Name'] + ' -- ' + ar[i]['Email']
+      string += '<br>'
+      email_list = email_list + ar[i]['Email'] + ', '
+    }
+  }
+  console.log(ar);
+   document.getElementById("available_ta_text").innerHTML = string;
+   document.getElementById("available_ta_email_list").innerHTML = email_list;
+}
 
-<!--  Charts Plugin -->
-<script src="assets/js/chartist.min.js"></script>
-
-<!--  Notifications Plugin    -->
-<script src="assets/js/bootstrap-notify.js"></script>
-
-<!--  Google Maps Plugin    -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
-
-<!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
-<script src="assets/js/light-bootstrap-dashboard.js"></script>
-
-<!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
-<script src="assets/js/demo.js"></script>
-
-<script type="text/javascript">
-$(document).ready(function () {
-
-    demo.initChartist();
-
-    $.notify({
-        icon: 'pe-7s-ribbon',
-        message: "Welcome to <b>Scu Labs</b> "
-
-    }, {
-        type: 'info',
-        timer: 4000
-    });
-
+document.getElementById("MonLab1").addEventListener("click", function(){
+	  myFunction("MonLab1")
 });
+document.getElementById("MonLab2").addEventListener("click", function(){
+	  myFunction("MonLab2")
+});
+document.getElementById("MonLab3").addEventListener("click", function(){
+	  myFunction("MonLab3")
+});
+
+document.getElementById("TueLab1").addEventListener("click", function(){
+	  myFunction("TueLab1")
+});
+document.getElementById("TueLab2").addEventListener("click", function(){
+	  myFunction("TueLab2")
+});
+document.getElementById("TueLab3").addEventListener("click", function(){
+	  myFunction("TueLab3")
+});
+
+document.getElementById("WedLab1").addEventListener("click", function(){
+	  myFunction("WedLab1")
+});
+document.getElementById("WedLab2").addEventListener("click", function(){
+	  myFunction("WedLab2")
+});
+document.getElementById("WedLab3").addEventListener("click", function(){
+	  myFunction("WedLab3")
+});
+
+document.getElementById("ThuLab1").addEventListener("click", function(){
+	  myFunction("ThuLab1")
+});
+document.getElementById("ThuLab2").addEventListener("click", function(){
+	  myFunction("ThuLab2")
+});
+document.getElementById("ThuLab3").addEventListener("click", function(){
+	  myFunction("ThuLab3")
+});
+
+document.getElementById("FriLab1").addEventListener("click", function(){
+	  myFunction("FriLab1")
+});
+document.getElementById("FriLab2").addEventListener("click", function(){
+	  myFunction("FriLab2")
+});
+document.getElementById("FriLab3").addEventListener("click", function(){
+	  myFunction("FriLab3")
+});
+
+
+
+
 </script>
+
+
+
+
+
 
 </html>
