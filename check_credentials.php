9da8d15c744f3b30e8e12c63cfc336be48471f97
Casema919 <cmaggio@scu.edu>
2017-10-29 15:15:09 -0700

    Add files via upload
    php pages from login and register

---
+++ check_credentials.php
@@ -0,0 +1,46 @@
+<?php
+	
+	$flag = true;
+
+	//if (isset($_POST['scu_login'])) {
+		
+		// eliminate white space  and prevent mysql injection from email
+		$uname = trim($_POST['uname']);
+		$uname = strip_tags($uname);
+		$uname = htmlspecialchars($uname);
+
+		// eliminate white space and prevent mysql injection from password
+		$pass = trim($_POST['pass']);
+		$pass = strip_tags($pass);
+		$pass = htmlspecialchars($pass);
+
+
+		if (empty($uname) or !filter_var($uname,FILTER_VALIDATE_EMAIL) ) {
+			$flag = false;
+			echo "Please enter valid email address";
+		} 
+		
+		if (empty($pass)){
+			$flag = false;
+			echo "Please enter password";
+		}
+
+		// if no error inputting information, continue
+		if ($flag) {
+
+			// fetch password and name from mysql Database where Email == entered email: uname
+			$sql_query = mysql_query("SELECT Pass, Name FROM Users u WHERE u.Email='$uname'");
+			$row = mysql_fetch_array($sql_query);
+			$num_rows = mysql_num_rows($sql_query);
+			
+			// if only one user in Database with provided information and passwords match, login valid
+			if (!($num_rows == 1 && $row['Pass'] == $pass)) { 
+				echo "Invalid Login Information"			
+				//invalid user, error out *** NEED TO IMPLEMENT ***
+			}
+		}
+
+	//}
+?>
+
+
---
+++ create_user.php
@@ -0,0 +1,85 @@
+<?php
+	
+	$flag = true;
+
+	//if (isset($_POST['signup'])) {
+		
+		// eliminate white space, NULL bytes, and unneccessary characters from name
+		$name = trim($_POST['name']);
+		$name = strip_tags($name);
+		$name = htmlspecialchars($name);
+		
+		// eliminate white space, NULL bytes, and unneccessary characters from email
+		$uname = trim($_POST['uname']);
+		$uname = strip_tags($uname);
+		$uname = htmlspecialchars($uname);
+		
+		// eliminate white space, NULL bytes, and unneccessary characters from password
+		$pass = trim($_POST['pass']);
+		$pass = strip_tags($pass);
+		$pass = htmlspecialchars($pass);
+
+		// eliminate white space, NULL bytes, and unneccessary characters from password 2
+		$pass2 = trim($_POST['pass2']);
+		$pass2 = strip_tags($pass2);
+		$pass2 = htmlspecialchars($pass2);
+
+		
+		if (empty($uname) or !filter_var($uname,FILTER_VALIDATE_EMAIL) ) {
+			$flag = false;
+			echo "Please enter valid email address";
+		} else {
+			$sql_query = mysql_query("SELECT email FROM Users u WHERE u.Email='$uname'")
+			$num_rows = mysql_num_rows($sql_query);
+			
+			// if there are any other users with the same email, error out
+			if ($num_rows != 0) {
+				$flag = false;
+				echo "Email already in use";
+				// *** NEED TO IMPLEMENT, EMAIL ALREADY IN USE ***
+			}
+		}		
+		
+		// if any of input are empty or passwords don't match, error out
+		if (empty($name)) {
+			$flag = false;
+			echo "Please enter Name";
+		}	
+
+		if (empty($pass)){
+			$flag = false;
+			echo "Please enter Password";
+		}
+
+		if (empty($pass2)){
+			$flag = false;
+			echo "Please enter Retype Password";
+		}
+
+		if ('$pass' != '$pass2') {
+			echo "Passwords do not match"
+			// *** NEED TO IMPLEMENT, Passwords dont match, error out ***
+		}
+
+
+		// if no errors, input new user
+		if ($flag) {
+			$sql_query = mysql_query("INSERT INTO Users(Email, Pass, Name) VALUES('$uname', '$pass', '$name')"); 
+			
+			if ($sql_query) {
+				echo "Login successful, please login now";
+				// reset variables
+				unset($name);
+				unset($email);
+				unset($pass);
+			} else {
+				echo "Login not sucessful, please try again";
+				// *** NEED TO IMPLEMENT, RETURN TO LOGIN PAGE ***	
+			}
+
+		}
+
+//	}
+
+?>
+
