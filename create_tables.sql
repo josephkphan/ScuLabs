f6da7aa2d2aadc03db848447081532cf7b0ab64d
josephkphan <josephkphan@gmail.com>
2017-11-24 13:26:12 -0800

    updated .sql files

--- create_tables.sql
+++ create_tables.sql
@@ -21,26 +21,6 @@ CREATE TABLE User (
 );
 
 
-INSERT INTO  User(Email, Pass, Name, Type, MonLab1, MonLab2, MonLab3, TueLab1, TueLab2, TueLab3, WedLab1, WedLab2, WedLab3, ThuLab1, ThuLab2, ThuLab3, FriLab1, FriLab2, FriLab3)
-VALUES ('jphan123@scu.edu','admin', 'Joey Phan', 'TA', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available' );
-
-INSERT INTO  User(Email, Pass, Name, Type, MonLab1, MonLab2, MonLab3, TueLab1, TueLab2, TueLab3, WedLab1, WedLab2, WedLab3, ThuLab1, ThuLab2, ThuLab3, FriLab1, FriLab2, FriLab3)
-VALUES ('cmaggio123@scu.edu','admin', 'Carl Maggio', 'TA', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available' );
-
-INSERT INTO  User(Email, Pass, Name, Type, MonLab1, MonLab2, MonLab3, TueLab1, TueLab2, TueLab3, WedLab1, WedLab2, WedLab3, ThuLab1, ThuLab2, ThuLab3, FriLab1, FriLab2, FriLab3)
-VALUES ('kduddi123@scu.edu','admin', 'Kirti Duddi', 'TA', 'Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available' );
-
-INSERT INTO  User(Email, Pass, Name, Type, MonLab1, MonLab2, MonLab3, TueLab1, TueLab2, TueLab3, WedLab1, WedLab2, WedLab3, ThuLab1, ThuLab2, ThuLab3, FriLab1, FriLab2, FriLab3)
-VALUES ('admin','admin', 'System Admin', 'Admin', 'Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available', 'Not Available' );
-
-
-
 CREATE TABLE EnabledUser (
     Email varchar(255)  PRIMARY KEY
 );
-
-INSERT INTO  EnabledUser(Email)
-VALUES ('jphan123@scu.edu');
-
-INSERT INTO  EnabledUser(Email)
-VALUES ('admin');
