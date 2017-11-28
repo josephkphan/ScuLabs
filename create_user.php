4dc854c5b96861f4987b33d73e31964e3be393fb
Casema919 <cmaggio@scu.edu>
2017-11-28 01:31:26 -0800

    Add files via upload
    I added alerts to these files. create_user and check_credentials definitely work, but I would test admin just to be sure.

--- create_user.php
+++ create_user.php
@@ -39,7 +39,7 @@
 
 		if (empty($uname) or !filter_var($uname,FILTER_VALIDATE_EMAIL) ) {
 			$flag = false;
-			echo "Please enter valid email address";
+			echo "<script>alert('Please enter valid email address');window.location.href='register.php';</script>";
 		} else {
 			//$query = "SELECT email FROM Users u where u.Email='$uname'";
 			$sql_query = $conn->query("SELECT Email FROM User u where u.Email='$uname'");
@@ -49,7 +49,7 @@
 			// if there are any other users with the same email, error out
 			if ($num_rows != 0) {
 				$flag = false;
-				echo "Email already in use";
+				echo "<script>alert('Email already in use');window.location.href='register.php';</script>";
 			}
 
 		}
@@ -57,22 +57,22 @@
 		// if any of input are empty or passwords don't match, error out
 		if (empty($name)) {
 			$flag = false;
-			echo "Please enter Name";
+			echo "<script>alert('Please enter Name');window.location.href='register.php';</script>";
 		}
 
 		if (empty($pass)){
 			$flag = false;
-			echo "Please enter Password";
+			echo "<script>alert('Please enter Password');window.location.href='register.php';</script>";
 		}
 
 		if (empty($pass2)){
 			$flag = false;
-			echo "Please enter Retype Password";
+			echo "<script>alert('Please Retype Password');window.location.href='register.php';</script>";
 		}
 
 		if ($pass != $pass2) {
-			echo "Passwords do not match";
-			// *** NEED TO IMPLEMENT, Passwords dont match, error out ***
+			$flag = false;
+			echo "<script>alert('Passwords do not match');window.location.href='register.php';</script>";
 		}
 
 
@@ -82,13 +82,10 @@
 			$sql_query = $conn->query("INSERT INTO User(Email, Pass, Name, Type, MonLab1, MonLab2, MonLab3, TueLab1, TueLab2, TueLab3, WedLab1, WedLab2, WedLab3, ThuLab1, ThuLab2, ThuLab3, FriLab1, FriLab2, FriLab3) VALUES('$uname', '$pass', '$name', 'TA', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available', 'Available')");
 
 			if ($sql_query) {
-				echo "Registration successful, please login now";
+				echo "<script>alert('Registration successful, please contact admin to validate');window.location.href='login.php';</script>";
 				// reset variables
-
-				header('Location: login.php?');
 			} else {
-				echo "Registration not sucessful, please try again";
-				// *** NEED TO IMPLEMENT, RETURN TO LOGIN PAGE ***
+				echo "<script>alert('Registration not successful, please try again.');window.location.href='login.php';</script>";
 			}
 
 		}
